8455003dec623c6dbd4f1f8d3f8f4178aee1aaa0
het patel <hppatidar12d@gmail.com>
2024-09-27 23:34:11 +0530

    update option for better boilerplate

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -3,30 +3,98 @@
 import { fileURLToPath } from "url";
 import fs from "fs-extra";
 import { dirname, join } from "path";
+import prompts from "prompts";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+(async () => {
+  const response = await prompts([
+    {
+      type: "toggle",
+      name: "isTailwind",
+      message: "Do you want to use Tailwind Css?",
+      initial: true,
+      active: "Yes",
+      inactive: "No",
+    },
+    {
+      type: "toggle",
+      name: "isRedux",
+      message: "Do you want to use Redux?",
+      initial: true,
+      active: "Yes",
+      inactive: "No",
+    },
+    {
+      type: "toggle",
+      name: "isDocker",
+      message: "Do you want to use Docker?",
+      initial: true,
+      active: "Yes",
+      inactive: "No",
+    },
+  ]);
 
-const [, , projectName] = process.argv;
+  console.log(response);
 
-if (!projectName) {
-  console.error(red("Please specify the project name."));
-  process.exit(1);
-}
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
 
-const projectPath = join(process.cwd(), projectName);
-const templatePath = join(__dirname, "template");
+  const [, , projectName] = process.argv;
 
-if (fs.existsSync(projectPath)) {
-  console.error(`Directory ${projectName} already exists.`);
-  process.exit(1);
-}
+  if (!projectName) {
+    console.error(red("Please specify the project name."));
+    process.exit(1);
+  }
 
-try {
-  fs.copySync(templatePath, projectPath);
-  console.log(`Project ${projectName} created successfully.`);
-  console.log(`cd ${projectName} && docker-compose up --build`);
-} catch (err) {
-  console.error(`Failed to create project: ${err.message}`);
-  process.exit(1);
-}
+  const projectPath = join(process.cwd(), projectName);
+  const templatePath = join(__dirname, "template");
+
+  if (fs.existsSync(projectPath)) {
+    console.error(`Directory ${projectName} already exists.`);
+    process.exit(1);
+  }
+
+  try {
+    fs.copySync(templatePath, projectPath);
+    if (!response.isTailwind) {
+      const tailwindFile = [
+        join(projectPath, "tailwind.config.ts"),
+        join(projectPath, "postcss.config.mjs"),
+      ];
+
+      tailwindFile.forEach((file) => {
+        if (fs.existsSync(file)) {
+          fs.removeSync(file);
+        }
+      });
+
+      const packageJson = join(projectPath, "package.json");
+      const packageJsonData = fs.readJSONSync(packageJson);
+      if (packageJsonData?.devDependencies?.tailwindcss) {
+        delete packageJsonData.devDependencies["tailwindcss"];
+      }
+      if (packageJsonData?.devDependencies?.postcss) {
+        delete packageJsonData.devDependencies["postcss"];
+      }
+      fs.writeJSONSync(packageJson, packageJsonData);
+    }
+
+    if (!response.isDocker) {
+      const dockerFiles = [
+        join(projectPath, "Dockerfile"),
+        join(projectPath, "Dockerfile.prod"),
+        join(projectPath, "docker-compose.yml"),
+        join(projectPath, "docker-compose.prod.yml"),
+      ];
+
+      dockerFiles.forEach((file) => {
+        if (fs.existsSync(file)) {
+          fs.removeSync(file);
+        }
+      });
+    }
+    console.log(`Project ${projectName} created successfully.`);
+  } catch (err) {
+    console.error(`Failed to create project: ${err.message}`);
+    process.exit(1);
+  }
+})();
